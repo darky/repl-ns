a9193ffc07d6ba841dbbbae89cd63c0eb35c13a9
Vladislav Botvin <darkvlados@gmail.com>
2022-12-03 19:35:59 +0300

    fix first init object

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-const namespaces: Record<string, object> = {}
+const namespaces: Record<string, Record<string, unknown>> = {}
 
 export const ns = <T extends object>(
   name: string,
@@ -14,12 +14,17 @@ export const ns = <T extends object>(
     await opts.before(namespaces[name] as T)
 
     const rewriteKeysIdx = opts.rewriteKeys.reduce((acc, k) => ({ ...acc, [k]: true }), {} as Record<string, boolean>)
+    namespaces[name] ??= {}
 
     await Promise.resolve(props)
       .then(p => Object.entries(p))
-      .then(es => es.filter(([k, v]) => opts.forceRewrite || rewriteKeysIdx[k] || typeof v === 'function'))
+      .then(es =>
+        es.filter(
+          ([k, v]) => opts.forceRewrite || rewriteKeysIdx[k] || typeof v === 'function' || namespaces[name]![k] == null
+        )
+      )
       .then(es => Object.fromEntries(es))
-      .then(p => ((namespaces[name] ??= {}), Object.assign(namespaces[name]!, p)))
+      .then(p => Object.assign(namespaces[name]!, p))
 
     await opts.after(namespaces[name] as T)
   })()
