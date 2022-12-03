1e6c6f2901e342a47c91d363b6e1ee9f2403c2c3
Vladislav Botvin <darkvlados@gmail.com>
2022-12-03 19:42:07 +0300

    fix typing

--- index.ts
+++ index.ts
@@ -3,30 +3,31 @@ const namespaces: Record<string, Record<string, unknown>> = {}
 export const ns = <T extends object>(
   name: string,
   props: T,
-  opts: {
-    before: (props?: T) => Promise<unknown>
-    after: (props: T) => Promise<unknown>
-    forceRewrite: boolean
-    rewriteKeys: string[]
-  } = { forceRewrite: false, rewriteKeys: [], async before() {}, async after() {} }
+  opts?: {
+    before?: (props?: T) => Promise<unknown>
+    after?: (props: T) => Promise<unknown>
+    forceRewrite?: boolean
+    rewriteKeys?: string[]
+  }
 ) => {
   const ready = (async () => {
-    await opts.before(namespaces[name] as T)
+    await opts?.before?.(namespaces[name] as T)
 
-    const rewriteKeysIdx = opts.rewriteKeys.reduce((acc, k) => ({ ...acc, [k]: true }), {} as Record<string, boolean>)
+    const rewriteKeysIdx =
+      opts?.rewriteKeys?.reduce((acc, k) => ({ ...acc, [k]: true }), {} as Record<string, boolean>) ?? {}
     namespaces[name] ??= {}
 
     await Promise.resolve(props)
       .then(p => Object.entries(p))
       .then(es =>
         es.filter(
-          ([k, v]) => opts.forceRewrite || rewriteKeysIdx[k] || typeof v === 'function' || namespaces[name]![k] == null
+          ([k, v]) => opts?.forceRewrite || rewriteKeysIdx[k] || typeof v === 'function' || namespaces[name]![k] == null
         )
       )
       .then(es => Object.fromEntries(es))
       .then(p => Object.assign(namespaces[name]!, p))
 
-    await opts.after(namespaces[name] as T)
+    await opts?.after?.(namespaces[name] as T)
   })()
 
   return Object.assign(() => namespaces[name] as T, { ready })
