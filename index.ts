8ab5447e8df44bcc544fa5a56dac62e877b131ce
Vladislav Botvin <darkvlados@gmail.com>
2022-12-03 18:50:48 +0300

    first sketch

---
+++ index.ts
@@ -0,0 +1,26 @@
+const namespaces: Record<string, object> = {}
+
+export const ns = async <T extends object>(
+  name: string,
+  opts: {
+    before: (props?: T) => Promise<unknown>
+    after: (props: T) => Promise<unknown>
+    forceRewrite: boolean
+    rewriteKeys: string[]
+  } = { forceRewrite: false, rewriteKeys: [], async before() {}, async after() {} },
+  props: T
+) => {
+  await opts.before(namespaces[name] as T)
+
+  const rewriteKeysIdx = opts.rewriteKeys.reduce((acc, k) => ({ ...acc, [k]: true }), {} as Record<string, boolean>)
+
+  await Promise.resolve(props)
+    .then(p => Object.entries(p))
+    .then(es => es.filter(([k, v]) => opts.forceRewrite || rewriteKeysIdx[k] || typeof v === 'function'))
+    .then(es => Object.fromEntries(es))
+    .then(p => ((namespaces[name] ??= {}), Object.assign(namespaces[name]!, p)))
+
+  await opts.after(namespaces[name] as T)
+
+  return () => namespaces[name] as T
+}
