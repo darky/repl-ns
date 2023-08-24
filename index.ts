a297862fbfe9860b65be6cef048df273edd0287d
Vladislav Botvin <darkvlados@gmail.com>
2023-08-24 21:21:21 +0300

    option sync

--- index.ts
+++ index.ts
@@ -8,31 +8,35 @@ export const ns = <T extends object>(
     after?: (props: T) => Promise<unknown>
     forceRewrite?: boolean
     rewriteKeys?: (keyof T)[]
+    sync?: boolean
   }
 ) => {
-  const ready = (async () => {
-    await options?.before?.(namespaces[name] as T)
-
+  const core = () => {
     const rewriteKeysIdx: Record<string, boolean> = {}
     options?.rewriteKeys?.forEach(k => (rewriteKeysIdx[k as string] = true))
     namespaces[name] ??= {}
 
-    await Promise.resolve(props)
-      .then(p => Object.entries(p))
-      .then(es =>
-        es.filter(
-          ([key, value]) =>
-            options?.forceRewrite ||
-            rewriteKeysIdx[key] ||
-            typeof value === 'function' ||
-            namespaces[name]![key] == null
-        )
-      )
-      .then(es => Object.fromEntries(es))
-      .then(p => Object.assign(namespaces[name]!, p))
+    const entries = Object.entries(props)
+    const newEntries = entries.filter(
+      ([key, value]) =>
+        options?.forceRewrite || rewriteKeysIdx[key] || typeof value === 'function' || namespaces[name]![key] == null
+    )
 
-    await options?.after?.(namespaces[name] as T)
-  })()
+    Object.assign(namespaces[name]!, Object.fromEntries(newEntries))
+  }
+
+  const ready = options?.sync
+    ? (() => {
+        options?.before?.(namespaces[name] as T)
+        core()
+        options?.after?.(namespaces[name] as T)
+        return Promise.resolve()
+      })()
+    : (async () => {
+        await options?.before?.(namespaces[name] as T)
+        core()
+        await options?.after?.(namespaces[name] as T)
+      })()
 
   return Object.assign(() => namespaces[name] as T, { ready })
 }
--- test.ts
+++ test.ts
@@ -15,6 +15,21 @@ test('basic', async () => {
   assert.strictEqual(n, 2)
 })
 
+test('sync basic', async () => {
+  const namespace = ns(
+    'sync.basic',
+    {
+      fn() {
+        return 1 + 1
+      },
+    },
+    { sync: true }
+  )
+
+  const n = namespace().fn()
+  assert.strictEqual(n, 2)
+})
+
 test('override fn', async () => {
   const namespace = ns('override', {
     fn() {
@@ -33,6 +48,31 @@ test('override fn', async () => {
   assert.strictEqual(n, 4)
 })
 
+test('sync override fn', async () => {
+  const namespace = ns(
+    'sync.override',
+    {
+      fn() {
+        return 1 + 1
+      },
+    },
+    { sync: true }
+  )
+
+  ns(
+    'sync.override',
+    {
+      fn() {
+        return 2 + 2
+      },
+    },
+    { sync: true }
+  )
+
+  const n = namespace().fn()
+  assert.strictEqual(n, 4)
+})
+
 test('preserve object by default', async () => {
   const namespace = ns('preserve:obj', {
     obj: 1,
@@ -42,6 +82,23 @@ test('preserve object by default', async () => {
   await ns('preserve:obj', {
     obj: 2,
   }).ready
+
+  const n = namespace().obj
+  assert.strictEqual(n, 1)
+})
+
+test('sync preserve object by default', async () => {
+  const namespace = ns(
+    'sync.preserve:obj',
+    {
+      obj: 1,
+    },
+    { sync: true }
+  )
+
+  ns('sync.preserve:obj', {
+    obj: 2,
+  })
 
   const n = namespace().obj
   assert.strictEqual(n, 1)
@@ -65,6 +122,27 @@ test('force rewrite', async () => {
   assert.strictEqual(n, 2)
 })
 
+test('sync force rewrite', async () => {
+  const namespace = ns(
+    'sync.force:rewrite',
+    {
+      obj: 1,
+    },
+    { sync: true }
+  )
+
+  ns(
+    'sync.force:rewrite',
+    {
+      obj: 2,
+    },
+    { forceRewrite: true, sync: true }
+  )
+
+  const n = namespace().obj
+  assert.strictEqual(n, 2)
+})
+
 test('rewrite by keys', async () => {
   const namespace = ns('rewrite:by:keys', {
     obj: 1,
@@ -78,6 +156,27 @@ test('rewrite by keys', async () => {
     },
     { rewriteKeys: ['obj'] }
   ).ready
+
+  const n = namespace().obj
+  assert.strictEqual(n, 2)
+})
+
+test('sync rewrite by keys', async () => {
+  const namespace = ns(
+    'sync.rewrite:by:keys',
+    {
+      obj: 1,
+    },
+    { sync: true }
+  )
+
+  ns(
+    'sync.rewrite:by:keys',
+    {
+      obj: 2,
+    },
+    { rewriteKeys: ['obj'], sync: true }
+  )
 
   const n = namespace().obj
   assert.strictEqual(n, 2)
@@ -111,6 +210,36 @@ test('before', async () => {
   assert.strictEqual(called, 2)
 })
 
+test('sync before', () => {
+  let called = 0
+
+  ns(
+    'sync.before',
+    { obj: 1 },
+    {
+      async before(props) {
+        called += 1
+        assert.strictEqual(props, void 0)
+      },
+      sync: true,
+    }
+  )
+
+  ns(
+    'sync.before',
+    { obj: 1 },
+    {
+      sync: true,
+      async before(props) {
+        called += 1
+        assert.strictEqual(props?.obj, 1)
+      },
+    }
+  )
+
+  assert.strictEqual(called, 2)
+})
+
 test('after', async () => {
   let called = 0
 
@@ -124,6 +253,24 @@ test('after', async () => {
       },
     }
   ).ready
+
+  assert.strictEqual(called, 1)
+})
+
+test('sync after', async () => {
+  let called = 0
+
+  ns(
+    'sync.after',
+    { obj: 1 },
+    {
+      async after(props) {
+        called += 1
+        assert.strictEqual(props?.obj, 1)
+      },
+      sync: true,
+    }
+  )
 
   assert.strictEqual(called, 1)
 })
